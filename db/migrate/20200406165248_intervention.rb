be5e747ebacbddf7695d06cdb7686db26584e6db
maxparent <maxparent7@gmail.com>
2020-04-07 15:36:23 -0400

    5

--- db/migrate/20200406165248_intervention.rb
+++ db/migrate/20200406165248_intervention.rb
@@ -1,20 +1,19 @@
 class Intervention < ActiveRecord::Migration[6.0]
-  def change
-    create_table :interventions do |t|
-      t.references :author, foreign_key: true
-      t.references :customer, foreign_key: true
-      t.references :building, foreign_key: true
-      t.references :battery, foreign_key: true
-      t.references :column, foreign_key: true
-      t.references :elevator, foreign_key: true
-      t.references :employee, foreign_key: true
-      t.datetime :start_date
-      t.datetime :end_date
-      t.string :result
-      t.string :company_name
-      t.text :report
-      t.string :status
-  
-      t.timestamps
+    def change
+      create_table :interventions do |t|
+        # t.references :author, foreign_key: true     
+        t.references :customer, foreign_key: true
+        t.references :building, foreign_key: true
+        t.references :battery, foreign_key: true
+        t.references :column, foreign_key: true
+        t.references :elevator, foreign_key: true
+        t.references :employee, foreign_key: true
+        t.datetime :start_date
+        t.datetime :end_date
+        t.string :result
+        t.text :report
+        t.string :status
+        t.timestamps
+    end
   end
 end
