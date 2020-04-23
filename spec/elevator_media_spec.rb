61e485f8ca21174ed9e849a9b99b2179d66ef3df
maxparent <maxparent7@gmail.com>
2020-04-23 16:07:57 -0400

    ca avance

--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -3,28 +3,27 @@ require 'elevator_media'
 describe Streamer do
 
     describe "get content" do
-# ---------Test get content--------------
-        context "Test to get content" do
-            it 'getting content' do
-                expect(Streamer.getcontent()).to eq("Hello World")
+        
+# ---------Test API open_weather-------------
+        context "Test to get realtime weather" do
+            it "getting weather in realtime" do
+                    expect(Streamer.getcontent()).to_not eq(nil)
             end
         end
+
 # ---------Test get an image-------------
         context "test to get image" do
             it "getting image" do
                 expect(Streamer.getimage()).to eq("app/assets/images/award1.jpeg")
             end
         end
-# ---------Test API open_weather-------------
-        context "Test to get realtime weather" do
-            it "getting weather in realtime" do
-                expect(Streamer.realtimeweather()).to_not eq(nil)
-            end
-        end
-        context "Test to get vimeo" do
-            it "getting vimeo" do
-                expect(Streamer.vimeovideo()).to_not eq(nil)
+
+# ---------Test get content--------------
+        context "Test to something" do
+            it 'getting something' do
+                expect(Streamer.getsomething()).to eq("Hello World")
             end
         end
     end 
 end
+
