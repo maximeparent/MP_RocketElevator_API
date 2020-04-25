1ada0bed86fed9836513a7a98e57c5d2e39a12bb
maxparent <maxparent7@gmail.com>
2020-04-24 21:10:57 -0400

    rspec-railsMarchePas

--- lib/elevator_media.rb
+++ lib/elevator_media.rb
@@ -1,39 +1,68 @@
 require 'open_weather'
 require 'http'
 require 'json'
+require 'mysql2'
 # module elevator_media
 
     class Streamer
         def self.add(input)
             input
         end
+# ---------Test in database Count employees, elevator, building----------------------
+        def self.getsomethingindb
+            client = Mysql2::Client.new(:host => "codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com", :username => "codeboxx", :password => "Codeboxx1!", :database => "MaximeParent")
+                results_employees = client.query("
+                    SELECT * FROM employees
+                    ")
+                results_elevators = client.query("
+                    SELECT * FROM elevators
+                    ")
+                results_buildings = client.query("
+                    SELECT * FROM buildings
+                    ")
+                    #number of employess, elevators and building
+            return " #{results_employees.count} #{results_elevators.count}  #{results_buildings.count}"
+        end
 
-        def self.getcontent
+
+# ---------Test get uselessfacts -----------------
+        def self.getuselessfacts
             uselessfact = HTTP.get("https://uselessfacts.jsph.pl/random.json?language=en")
             obj = JSON.parse(uselessfact)['text']
-            html = "<html><body><div> #{obj} </div></body></html>"
+            html = " #{obj} "
             puts html
             return html
         end
-    # ---------Test get weather --------------
-        
+
+# ---------Test get weather ----------------------
         def self.getweather()
             options = { units: "metric", APPID: '2a85b82a44affaa68c36bf2b7423be21' }
             info = OpenWeather::Current.city("Los Angeles, US", options)
             puts info
             info
         end
-
-        
-    # ---------Test Get an image-------------
+# ---------Test Get an image-----------------------
         def self.getimage()
             "app/assets/images/award1.jpeg"
         end
-    # ---------Test get something--------------
-        def self.getsomething()
-            return "Hello World"
+        
+# # ---------Test get something----------------------
+#         def self.getsomething()
+#             return "Hello World"
+#         end
+# ---------Test get superhero-----------------------
+        def self.getsuperhero()
+            testurl =  Net::HTTP.get(URI("https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/28.json"))
+                superhero = JSON.parse(testurl)
+                return superhero["name"]
+        end
+
+# -------------Get content--------------------------
+        def self.getcontent()
+            puts "<div> #{getweather()} </div>"
+            puts "<div> #{getuselessfacts()} </div>"
+            puts "<div> #{getsuperhero()} </div>"
+            return "<div> #{getuselessfacts()} </div>"
         end
     end
 
-
-    
--- spec/elevator_media_spec.rb
+++ spec/elevator_media_spec.rb
@@ -1,33 +1,54 @@
 require 'elevator_media'
-
 describe Streamer do
+    # ---------Test in database------
+    context "Test Get something in db" do
+        it "Test (1) if we connect with db " do
+        end
+        it "Test (2) if we connect with db " do
+            expect(Streamer.getsomethingindb()).to_not eq(nil)
+        end
+        it "Test if we connect with table's db"do
+            expect(Streamer.getsomethingindb()).to_not eq(nil)
+        end
+        it "test if we can fetch data from a table" do
+            expect(Streamer.getsomethingindb()).to_not eq(nil)
+        end
+        it "test if we can fetch data from a table as a string" do
+            expect(Streamer.getsomethingindb()).to be_a(String)
+        end
+    end
 
-    describe "get content" do
 # ---------Test get useless facts-------------
-        context "get a useless facts" do
-            it "return a fun quote as a string" do
-                    expect(Streamer.getcontent).to be_kind_of String
-            end
+        
+    context "get a useless facts" do
+        it "return a fun quote as a string" do
+                expect(Streamer.getuselessfacts).to be_kind_of String
         end
+    end
 # ---------Test API open_weather-------------
-        context "Test to get realtime weather" do
-            it "getting weather in realtime" do
-                    expect(Streamer.getweather()).to_not eq(nil)
-            end
+    context "Test to get realtime weather" do
+        it "getting weather in realtime" do
+            expect(Streamer.getweather()).to_not eq(nil)
         end
+    end
 
 # ---------Test get an image-------------
-        context "test to get image" do
-            it "getting image" do
-                expect(Streamer.getimage()).to eq("app/assets/images/award1.jpeg")
-            end
+    context "test to get image" do
+        it "getting image" do
+            expect(Streamer.getimage()).to eq("app/assets/images/award1.jpeg")
         end
+    end
 
-# ---------Test get something--------------
-        context "Test to something" do
-            it 'getting something' do
-                expect(Streamer.getsomething()).to eq("Hello World")
-            end
+# # ---------Test get something--------------
+#         context "Test to get something" do
+#             it 'getting something' do
+#                 expect(Streamer.getsomething()).to eq("Hello World")
+#             end
+        # end
+# ---------Test get superhero--------------
+        describe "Getting superhero name" do
+            it "superhero name:" do
+                expect(Streamer.getsuperhero()).to be_a(String)
         end
     end
 end
